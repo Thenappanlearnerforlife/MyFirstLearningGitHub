e5f771faab58e030cddb60086a02d31d7848755a
Thenappanlearnerforlife <58080850+Thenappanlearnerforlife@users.noreply.github.com>
2019-11-23 19:48:12 +0530

    NewBranch
    New Statement is added in the NewModuleBranch module

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,7 @@ public static void main(String [] args)
 
 System.out.println("Welcome to Jenkins Tutorial");
 System.out.println("File is edited");
+System.out.println("New Branch is created: NewModuleBranch");
 
 }
 
